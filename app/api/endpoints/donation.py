c71b0da47fdc35bdea961ad5c7f72eccd6e754ec
babichdenis <babichdenis@me.com>
2024-08-12 11:52:06 +0300

    No_Bisness_Logic

--- app/api/endpoints/donation.py
+++ app/api/endpoints/donation.py
@@ -4,12 +4,17 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.db import get_async_session
 from app.core.user import current_user, current_superuser
 from app.crud.donation import donation_crud
-from app.models.charity_project import CharityProject
+from app.crud.charity_project import charity_project_crud
 from app.models.user import User
 from app.schemas.donation import DonationCreate, DonationDB, DonationDBAll
 from app.services.investment import process_donation
 
 router = APIRouter()
+
+
+async def get_charity_project_from_database(session: AsyncSession):
+    """Get a CharityProject object from the database."""
+    return await charity_project_crud.get_single(session)
 
 
 @router.post(
@@ -31,7 +36,7 @@ async def create_new_donation(
     )
 
     # Получаем объект CharityProject из базы данных
-    charity_project = await get_charity_project_from_database()
+    charity_project = await get_charity_project_from_database(session)
     return await process_donation(
         new_donation,
         charity_project,
